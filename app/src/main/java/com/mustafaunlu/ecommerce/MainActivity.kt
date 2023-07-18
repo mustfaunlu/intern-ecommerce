1b80d5899e42729dbba99f88b5beab2771392e50
Mustafa Ünlü <38860392+mustfaunlu@users.noreply.github.com>
2023-07-18 20:37:30 +0300

    push notification implemented

--- app/src/main/java/com/mustafaunlu/ecommerce/MainActivity.kt
+++ app/src/main/java/com/mustafaunlu/ecommerce/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.mustafaunlu.ecommerce
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import com.mustafaunlu.ecommerce.databinding.ActivityMainBinding
 import com.mustafaunlu.ecommerce.utils.gone
 import com.mustafaunlu.ecommerce.utils.visible
@@ -14,25 +18,73 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(
+            OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.w("Fail", "Fetching FCM registration token failed", task.exception)
+                    return@OnCompleteListener
+                }
+
+                // Get new FCM registration token
+                val token = task.result
+
+                // Log and toast
+                val msg = token.toString()
+                Log.d("Token", msg)
+            },
+        )
+
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        NavigationUI.setupWithNavController(binding.bottomNavigation, navHostFragment.navController)
+        navController = navHostFragment.navController
+        setupWithNavController(binding.bottomNavigation, navController)
 
-        navHostFragment.navController.addOnDestinationChangedListener { _, destination, _ ->
+        navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
                 R.id.loginFragment -> {
                     binding.bottomNavigation.gone()
+                    supportActionBar?.hide()
                 }
-
+                R.id.homeFragment -> {
+                    binding.bottomNavigation.visible()
+                    supportActionBar?.hide()
+                }
+                R.id.cartFragment -> {
+                    binding.bottomNavigation.visible()
+                    setActionBarProperties(getString(R.string.title_shopping_cart), true)
+                }
+                R.id.profileFragment -> {
+                    binding.bottomNavigation.visible()
+                    setActionBarProperties(getString(R.string.title_profile), true)
+                }
+                R.id.detailFragment -> {
+                    binding.bottomNavigation.visible()
+                    setActionBarProperties(getString(R.string.title_product_info), true)
+                }
                 else -> {
                     binding.bottomNavigation.visible()
+                    supportActionBar?.show()
                 }
             }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+    private fun setActionBarProperties(title: String, showHomeButton: Boolean = false) {
+        supportActionBar?.apply {
+            this.title = title
+            setHomeButtonEnabled(showHomeButton)
+            setDisplayHomeAsUpEnabled(showHomeButton)
+            show()
         }
     }
 }
