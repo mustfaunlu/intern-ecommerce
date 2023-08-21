f0572ed75789b8ece6e084c0209220561b897c92
Mustafa Ünlü <38860392+mustfaunlu@users.noreply.github.com>
2023-08-21 09:34:28 +0300

    add singleton annotation

--- app/src/main/java/com/mustafaunlu/ecommerce/di/permission/PermissionModule.kt
+++ app/src/main/java/com/mustafaunlu/ecommerce/di/permission/PermissionModule.kt
@@ -6,12 +6,14 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(ActivityComponent::class)
 object PermissionModule {
 
     @Provides
+    @Singleton
     fun providePermissionManager(activity: Activity): InternetPermissionManager {
         return InternetPermissionManager(activity)
     }
